9af67e590b78b6c7d10bfa27b3d6b37141f827e4
yunchanpark <96051437+yunchanpark@users.noreply.github.com>
2021-12-19 20:46:30 +0900

    yunchan#2
    1. spring.jpa,hibernate.ddl-auto: creat 변경
    2. productRepositoryTest: 작성
    3. Spring Audit 사용하기 위해 빠트렸던 EnableJpaAuditing 어노테이션 추가
    4. layout/user/product에 write.html 추가

--- src/main/java/com/lec/foodmarket/FoodmarketApplication.java
+++ src/main/java/com/lec/foodmarket/FoodmarketApplication.java
@@ -2,8 +2,10 @@ package com.lec.foodmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FoodmarketApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/lec/foodmarket/repository/ProductRepositoryTest.java
+++ src/test/java/com/lec/foodmarket/repository/ProductRepositoryTest.java
@@ -1,5 +1,49 @@
 package com.lec.foodmarket.repository;
 
-public class ProductRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.lec.foodmarket.domain.Product;
+import com.lec.foodmarket.domain.ProductCategory;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private ProductCategoryRepository productCategoryRepository;
+	
+	@Test
+	void crud() {
+		System.out.println("\n--TEST#crud()------------------------------------------------");
+		ProductCategory category = ProductCategory.builder()
+				.name("채소")
+				.build();
+		
+		productCategoryRepository.save(category);
+		
+		Product product = Product.builder()
+				.categoryNo(category)
+				.description("명품사과입니다")
+				.detailContent("<br><br>")
+				.discount(10)
+				.imageOrgin("원본파일")
+				.imageSave("서버파일")
+				.name("박윤찬")
+				.price(2000)
+				.viewCnt(0)
+				.stock(100)
+				.purchasePrice(1000)
+				.build();
+		productRepository.save(product);
+		productRepository.findAll().forEach(System.out::println);
+		
+		
+		
+		
+		System.out.println("---------------------------------------------------------------");
+	}
 }
